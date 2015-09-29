1a1bffbbc4e0161824f8d6e8b068691d569da5e2
IfanRamza <ifan.ramadhanzaki@gmail.com>
2015-09-29 15:25:42 +0700

    Update Crawler.py

--- 3.-Web-Crawler/Crawler.py
+++ 3.-Web-Crawler/Crawler.py
@@ -1,9 +1,10 @@
 #! C:\python27
 
 import urllib
+import urllib2
 from bs4 import BeautifulSoup
 
-urllib.urlretrieve("http://detik.com/", "index.html")
+urllib.urlretrieve("http://detik.com/", "index.html") 
 soup = BeautifulSoup(open("index.html"))
 
 #Inisiasi
@@ -14,6 +15,6 @@ for anchor in soup.findAll('a', href=True):
     w = anchor['href']
     if ( str(w).startswith( 'http' )):   
    		print w
-   		x = str(i)+".html"
+   		x = str(i)+".html" #Menyimpan source code hyperlink ke dalam html
    		urllib.urlretrieve( w , x )
    		i = i+1
