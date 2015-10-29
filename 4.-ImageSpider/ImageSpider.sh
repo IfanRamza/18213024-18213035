ecb7a9e1907f4116df630103d0b393023a8d7da9
IfanRamza <ifan.ramadhanzaki@gmail.com>
2015-10-29 20:16:00 +0700

    Update ImageSpider.sh

--- 4.-ImageSpider/ImageSpider.sh
+++ 4.-ImageSpider/ImageSpider.sh
@@ -4,7 +4,7 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ddir="$dir/$1"
 b="backup_$1"
 
-wget -r   -A jpg,jpeg,gif,png $1
+wget -r   -A jpg,jpeg 'http://www.detik.com/'
 [ ! -d $b ] && mkdir $b
 	
 rsync -a  $ddir/ $b
